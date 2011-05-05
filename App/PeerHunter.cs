0a6fb494376eb19dec4888a91aba8dc3eabf067e
João Silvestre <joaopaulosilvestre@hotmail.com>
2011-05-05 16:27:54 +0100

    Implementação da pesquisa de novos peers.

---
+++ App/PeerHunter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Indexers;
+
+namespace App
+{
+    class PeerHunter
+    {
+        private int _timeBetweenSearches = 5000; //In ms
+        private const int IncrementAfterSearch = 2;
+        private const int MaxTimeBetweenSearches = 20*60*60*1000;
+
+        public void Hunt()
+        {
+            do
+            {
+                var container =
+                    Peer.Self.PeerContainer as PeerContainer;
+
+                if (container != null) 
+                    container.Synchronize();
+                else
+                    throw new InvalidProgramException(); //Can't be null.
+
+                Thread.Sleep(_timeBetweenSearches);
+
+                if (_timeBetweenSearches < MaxTimeBetweenSearches)
+                    _timeBetweenSearches += _timeBetweenSearches*IncrementAfterSearch;
+            } while (true);
+        }
+    }
+}
